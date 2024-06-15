9493ca175fadee1099c64167c4f4856861cd9e83
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-15 23:59:41 +0700

    [UPDATE] build header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,30 @@
+import { navLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return <nav>Header</nav>
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <Link href={'/'}>
+          <Image src='/header-logo.svg' alt='heder logo' width={130} height={29} />
+        </Link>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <Link href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <Image className='hidden max-lg:block ' src='/hamburger.svg' alt='humburger' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
---
+++ src/constants/index.ts
@@ -0,0 +1,120 @@
+export const navLinks = [
+  { href: '#home', label: 'Home' },
+  { href: '#about-us', label: 'About Us' },
+  { href: '#products', label: 'Products' },
+  { href: '#contact-us', label: 'Contact Us' }
+]
+
+export const shoes = [
+  {
+    thumbnail: '/thumbnail-shoe1.svg',
+    bigShoe: '/big-shoe1.png'
+  },
+  {
+    thumbnail: '/thumbnail-shoe2.svg',
+    bigShoe: '/big-shoe2.png'
+  },
+  {
+    thumbnail: '/thumbnail-shoe3.svg',
+    bigShoe: '/big-shoe3.png'
+  }
+]
+
+export const statistics = [
+  { value: '1k+', label: 'Brands' },
+  { value: '500+', label: 'Shops' },
+  { value: '250k+', label: 'Customers' }
+]
+
+export const products = [
+  {
+    imgURL: '/shoe4.svg',
+    name: 'Nike Air Jordan-01',
+    price: '$200.20'
+  },
+  {
+    imgURL: '/shoe5.svg',
+    name: 'Nike Air Jordan-10',
+    price: '$210.20'
+  },
+  {
+    imgURL: '/shoe6.svg',
+    name: 'Nike Air Jordan-100',
+    price: '$220.20'
+  },
+  {
+    imgURL: '/shoe7.svg',
+    name: 'Nike Air Jordan-001',
+    price: '$230.20'
+  }
+]
+
+export const services = [
+  {
+    imgURL: '/truck-fast.svg',
+    label: 'Free shipping',
+    subtext: 'Enjoy seamless shopping with our complimentary shipping service.'
+  },
+  {
+    imgURL: '/shield-tick.svg',
+    label: 'Secure Payment',
+    subtext: 'Experience worry-free transactions with our secure payment options.'
+  },
+  {
+    imgURL: '/support.svg',
+    label: 'Love to help you',
+    subtext: 'Our dedicated team is here to assist you every step of the way.'
+  }
+]
+
+export const reviews = [
+  {
+    imgURL: '/customer1.jpeg',
+    customerName: 'Morich Brown',
+    rating: 4.5,
+    feedback: 'The attention to detail and the quality of the product exceeded my expectations. Highly recommended!'
+  },
+  {
+    imgURL: '/customer2.svg',
+    customerName: 'Lota Mongeskar',
+    rating: 4.5,
+    feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+  }
+]
+
+export const footerLinks = [
+  {
+    title: 'Products',
+    links: [
+      { name: 'Air Force 1', link: '/' },
+      { name: 'Air Max 1', link: '/' },
+      { name: 'Air Jordan 1', link: '/' },
+      { name: 'Air Force 2', link: '/' },
+      { name: 'Nike Waffle Racer', link: '/' },
+      { name: 'Nike Cortez', link: '/' }
+    ]
+  },
+  {
+    title: 'Help',
+    links: [
+      { name: 'About us', link: '/' },
+      { name: 'FAQs', link: '/' },
+      { name: 'How it works', link: '/' },
+      { name: 'Privacy policy', link: '/' },
+      { name: 'Payment policy', link: '/' }
+    ]
+  },
+  {
+    title: 'Get in touch',
+    links: [
+      { name: 'customer@nike.com', link: 'mailto:customer@nike.com' },
+      { name: '+92554862354', link: 'tel:+92554862354' }
+    ]
+  }
+]
+
+export const socialMedia = [
+  { src: '/facebook.svg', alt: 'facebook logo' },
+  { src: '/twitter.svg', alt: 'twitter logo' },
+  { src: '/instagram.svg', alt: 'instagram logo' }
+]
